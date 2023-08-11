cd7f20a6c3c0c7a8bd82a4e5299439ad7cad8bb2
YOSEONG-globalin <spos423@gmail.com>
2023-08-11 18:24:48 +0900

    커밋커밋

---
+++ src/main/java/com/globalin/biz/eboard/E_BoardVO.java
@@ -0,0 +1,161 @@
+package com.globalin.biz.eboard;
+
+import java.sql.Date;
+
+public class E_BoardVO {
+	private int num;
+	private String title;
+	private String content;
+	private int zipcode;
+	private String address;
+	private String traf;
+	private String price;
+	private Date startdate;
+	private Date enddate;
+	private String tel;
+	private String uri;
+	private char deleteyn;
+	private String writer;
+	private Date writeday;
+	private String updater;
+	private Date updateday;
+	private int readcnt;
+	private String img1_url;
+	private String img2_url;
+	private String img3_url;
+	private String img4_url;
+	private String img5_url;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTraf() {
+		return traf;
+	}
+	public void setTraf(String traf) {
+		this.traf = traf;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public Date getStartdate() {
+		return startdate;
+	}
+	public void setStartdate(Date startdate) {
+		this.startdate = startdate;
+	}
+	public Date getEnddate() {
+		return enddate;
+	}
+	public void setEnddate(Date enddate) {
+		this.enddate = enddate;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getUri() {
+		return uri;
+	}
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+	public char getDeleteyn() {
+		return deleteyn;
+	}
+	public void setDeleteyn(char deleteyn) {
+		this.deleteyn = deleteyn;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public Date getWriteday() {
+		return writeday;
+	}
+	public void setWriteday(Date writeday) {
+		this.writeday = writeday;
+	}
+	public String getUpdater() {
+		return updater;
+	}
+	public void setUpdater(String updater) {
+		this.updater = updater;
+	}
+	public Date getUpdateday() {
+		return updateday;
+	}
+	public void setUpdateday(Date updateday) {
+		this.updateday = updateday;
+	}
+	public int getReadcnt() {
+		return readcnt;
+	}
+	public void setReadcnt(int readcnt) {
+		this.readcnt = readcnt;
+	}
+	public String getImg1_url() {
+		return img1_url;
+	}
+	public void setImg1_url(String img1_url) {
+		this.img1_url = img1_url;
+	}
+	public String getImg2_url() {
+		return img2_url;
+	}
+	public void setImg2_url(String img2_url) {
+		this.img2_url = img2_url;
+	}
+	public String getImg3_url() {
+		return img3_url;
+	}
+	public void setImg3_url(String img3_url) {
+		this.img3_url = img3_url;
+	}
+	public String getImg4_url() {
+		return img4_url;
+	}
+	public void setImg4_url(String img4_url) {
+		this.img4_url = img4_url;
+	}
+	public String getImg5_url() {
+		return img5_url;
+	}
+	public void setImg5_url(String img5_url) {
+		this.img5_url = img5_url;
+	}
+	
+}
